efba712247b27e94107d380f0e45416d2864ec1b
apple-guobk <baokui2011@163.COM>
2021-06-02 16:27:56 +0800

    convert

--- run_convert.sh
+++ run_convert.sh
@@ -1,3 +1,3 @@
-python keras_to_tensorflow.py 
-    --input_model="/search/odin/guobk/data/simcse/model/model_005.h5" 
+python keras_to_tensorflow.py \
+    --input_model="/search/odin/guobk/data/simcse/model/model_005.h5" \
     --output_model="/search/odin/guobk/data/simcse/model/model_005.pb"
